b96ca55c0c34a1554eb7ee9677075074aab7e684
kiruthiga <kiruthiga>
2025-07-22 17:23:34 +0530

    Created a program with only one class

---
+++ src/org/age/Age.java
@@ -0,0 +1,17 @@
+package org.age;
+import java.util.Scanner;
+public class Age {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the age");
+	int age=s.nextInt();
+	System.out.println("Enter the person age:"+age);
+	if (age==18) {
+		System.out.println("Person is elegible to vote");
+	} else {
+		System.out.println("Persobn is not elegible to vote");
+
+	}
+		
+}
+}
